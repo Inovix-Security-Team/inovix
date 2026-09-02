3e78da4c84419fa9a459db2589de9fbe7d09c167
Shaikh Abdul Basit <abdulbasitshaikh436@gmail.com>
2026-09-02 09:31:15 +0530

    Update import path for ThreatIntelligenceProvider

--- security-engine/threat_intelligence/__init__.py
+++ security-engine/threat_intelligence/__init__.py
@@ -1,4 +1,4 @@
-from threat_intelligence.base import ThreatIntelligenceProvider
+from security_engine.threat_intelligence.base import ThreatIntelligenceProvider
 from threat_intelligence.local import LocalThreatIntelligenceProvider
 
 __all__ = [
